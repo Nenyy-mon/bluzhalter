9ecc5fca41a45674b7cff506d5c34c2866c3ca1e
Nenyy-mon <shone2699@hotmail.rs>
2024-06-27 16:18:03 +0200

    gallery update fix7

--- src/Gallery/MiddleGallery.jsx
+++ src/Gallery/MiddleGallery.jsx
@@ -1,4 +1,5 @@
-const modules = import.meta.glob('/src/assets/images/smallerGall/*.{png,jpg,jpeg,PNG,JPEG}')
+const modules = import.meta.glob(`https://nenyy-mon.github.io/bluzhalter/src/assets/images/smallerGall/
+*.{png,jpg,jpeg,PNG,JPEG}`)
 
 const gallery = Object.keys(modules).map((path) => new URL(path, import.meta.url).href);
 import { Swiper,SwiperSlide } from "swiper/react"
